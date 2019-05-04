df51d86c161ad64e5f68bb86cc4c214c957361f8
Will <wilmc17@gmail.com>
2019-05-03 17:08:36 -0700

    fixed file naming

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ camera.resolution = (1280, 720)
 counter = 0  # counts number of video files
 while True:
     # Set file name for current video segment
-    segName = 'dash'+str(counter)+'.264'
+    segName = 'dash'+str(counter)+'.h264'
     # Start recording
     camera.start_recording(segName)
     print('now recording')
