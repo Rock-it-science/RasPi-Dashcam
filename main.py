df4d1db16d819d382f196db9df2284f8913a86e4
Will <wilmc17@gmail.com>
2019-05-03 17:06:09 -0700

    main file; only implemented recording in segments and naming

---
+++ main.py
@@ -0,0 +1,19 @@
+#Camera setup
+import picamera
+camera = picamera.PiCamera()
+camera.resolution = (1280, 720)
+
+counter = 0  # counts number of video files
+while True:
+    # Set file name for current video segment
+    segName = 'dash'+str(counter)+'.264'
+    # Start recording
+    camera.start_recording(segName)
+    print('now recording')
+    camera.wait_recording(5)  # Continue recording for 10 seconds
+    # Stop recording
+    camera.stop_recording()
+    print('recording stopped, and segment saved as '+segName)
+    counter += 1
+
+
